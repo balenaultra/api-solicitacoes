52a86fa6fffe34a8bc6e0ac819368497c7197365
Ivonei Balena <balenaultra@users.noreply.github.com>
2021-04-14 22:59:23 -0300

    push evento no put da requisicao

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,10 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/request');
-const authService = require('../services/auth');
+const authService = require('../services/auth'); 
 
 router.post('/', authService.isAdmin, controller.post);
-router.put('/:id', authService.isAdmin, controller.put);
+router.put('/:id', authService.authorize, controller.put, 
+(req, res, next) => {    
+    router.stream.push_sse(req.decoded.company_cpf_cnpj, "request", { event: req.body });
+}); 
 router.get('/', authService.authorize, controller.get);
 
 module.exports = router;
